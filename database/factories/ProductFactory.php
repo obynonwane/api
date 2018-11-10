232b591da980f0a127f259c41059ff9f4cafcfd8
obinna <biostechnologyng@gmail.com>
2018-11-10 21:35:36 +0100

    Model Factory Created

--- database/factories/ProductFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Model\Product::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
--- database/factories/ReviewFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Model\Review::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
